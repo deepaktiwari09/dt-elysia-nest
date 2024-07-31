00c0a99c11271076daf0dfd0d6b6a8a32499ea3f
deepak tiwari <deepaktiwari3020@gmail.com>
2024-08-01 00:46:02 +0530

    codebase file reduces

--- src/config/database.ts
+++ src/config/database.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectToDatabase = async () => {
   try {
-    let client = await mongoose.connect(
+    await mongoose.connect(
       `mongodb+srv://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@indiancluster.yynfnzm.mongodb.net/${process.env.DATABASE_NAME}?retryWrites=true&w=majority&appName=indianCluster`
     );
     console.log("====== MongoDB connected ======");
