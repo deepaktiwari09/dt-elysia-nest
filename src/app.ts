a3eeb94bcc6675f96a800bfc17e9f8ebbfc1f2fb
deepak tiwari <deepaktiwari3020@gmail.com>
2024-08-01 00:29:05 +0530

    miner changes

--- src/app.ts
+++ src/app.ts
@@ -1,34 +1,26 @@
 import { Elysia } from "elysia";
 import { connectToDatabase } from "./config/database";
-import { swagger } from '@elysiajs/swagger'
+import { swagger } from "@elysiajs/swagger";
 import organizationService from "./modules/organisations/organisation.service";
 import productService from "./modules/products/product.service";
 import skillService from "./modules/skills/skill.service";
 import userStoryService from "./modules/userStories/userStory.service";
 import organizationController from "./modules/organisations/organisation.controller";
-import logixlysia from 'logixlysia'
+import logixlysia from "logixlysia";
 import { logger } from "@chneau/elysia-logger";
 import { config } from "./config/logger";
+import { cors } from "@elysiajs/cors";
+import { staticPlugin } from '@elysiajs/static'
 
-connectToDatabase()
+connectToDatabase();
 
-const app = new Elysia().use(logixlysia(config))
-
-//controllers
-app.use(organizationController())
-
-
-app.use(swagger({
-  path:'/docs'
-}))
-
-
-
-
-
-//Start Server
-app.listen(3000,(server)=>{
-  console.log("server is started on localhost:3000")
-  console.log("server api docs available on localhost:3000/docs")
-})
-
+const app = new Elysia()
+  .use(cors())
+  .use(staticPlugin())
+  .use(logixlysia(config))
+  .use(organizationController())
+  .use(swagger({ path: "/docs" }))
+  .listen(3000, (server) => {
+    console.log("server is started on localhost:3000");
+    console.log("server api docs available on localhost:3000/docs");
+  });
